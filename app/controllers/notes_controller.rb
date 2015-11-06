26b5e8ca8b7301a6362cb14fd034b3950ae07eba
Aleksandr Vest <swest2462@gmail.com>
2015-11-06 13:39:40 +0300

    association between user and notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,11 +8,11 @@ before_action :find_note, only: [:show, :edit, :update, :destroy]
   end
 
   def new
-    @note = Note.new
+    @note = current_user.notes.build
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
 
     if @note.save
       redirect_to @note
